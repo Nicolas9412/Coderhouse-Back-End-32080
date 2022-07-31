774a79f2c894a04d9d47c5efb87e4bb5ee345529
Nicolas Olmos <nicolas1819991@gmail.com>
2022-07-31 19:02:39 -0300

    Cambio de puerto

--- Desafio-Servidor-Express/servidor.js
+++ Desafio-Servidor-Express/servidor.js
@@ -3,12 +3,19 @@ const contenedor = new Contenedor("productos.json");
 
 const express = require("express");
 const app = express();
-const server = app.listen(8080, () =>
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () =>
   console.log(
     `Servidor iniciado escuchando por el puerto ${server.address().port}`
   )
 );
 server.on("error", (error) => console.log(`Error en el servidor ${error}`));
+
+app.get("/", (req, res) => {
+  res.send(`Bienvenido a mi primera api !!!
+  GET /productos devuelve todos los productos
+  GET /productoRandom devuelve un producto random`);
+});
 
 app.get("/productos", async (req, res) => {
   const listProducts = await contenedor.getAll();
