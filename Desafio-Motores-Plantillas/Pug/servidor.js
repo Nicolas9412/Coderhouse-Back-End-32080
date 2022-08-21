23a582ea65261b9db6c9d2eb78d265203caabf3d
Nicolas Olmos <nicolas1819991@gmail.com>
2022-08-21 14:33:03 -0300

    5-Desafio-Motores-Plantillas

---
+++ Desafio-Motores-Plantillas/Pug/servidor.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const Contenedor = require("./Contenedor");
+const contenedor = new Contenedor("productos.json");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+const server = app.listen(8080, () => {
+  console.log(`Servidor http iniciado en el puerto ${server.address().port}`);
+});
+server.on("error", (error) => {
+  console.log(`Error en el servidor ${error}`);
+});
+
+app.get("/", (req, res) => {
+  res.render("formulario");
+});
+
+app.post("/productos", async (req, res) => {
+  const { body } = req;
+  const { title, price, thumbnail } = body;
+  const producto = { title, price: parseInt(price), thumbnail };
+  await contenedor.save(producto);
+  res.redirect("/");
+});
+
+app.get("/productos", async (req, res) => {
+  const productos = await contenedor.getAll();
+  if (productos.length !== 0) {
+    res.render("listadoProductos", { productos });
+  } else {
+    res.render("sinProductos");
+  }
+});
