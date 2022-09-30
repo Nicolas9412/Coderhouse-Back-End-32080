db5c3a865fca711cc0f2df5257cc57819e6aec2f
Nicolas Olmos <nicolas1819991@gmail.com>
2022-09-30 16:07:43 -0300

    agrego folders de ejs

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () =>
-  console.log(`Servidor levantado http://localhost:${PORT}`)
-);
+app.set("view engine", "ejs");
 
+const server = app.listen(PORT, () => {
+  console.log(`Servidor levantado http://localhost:${server.address().port}`);
+});
+server.on("error", (error) => {
+  console.log(`Error en el servidor ${error}`);
+});
 app.get("/", (req, res) => {
-  res.send("Hola");
+  res.render("/pages/login.ejs");
 });
