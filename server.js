3d20246e53a67e06048ee68fa8c059d323e14326
Nicolas Olmos <nicolas1819991@gmail.com>
2022-10-02 22:15:23 -0300

    conexion con Mongo Atlas

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 const app = express();
 const PORT = 8080;
+
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl: "",
+      mongoOptions: advancedOptions,
+    }),
+    secret: "top secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +28,24 @@ const server = app.listen(PORT, () => {
 server.on("error", (error) => {
   console.log(`Error en el servidor ${error}`);
 });
-app.get("/", (req, res) => {
-  res.render("/pages/login.ejs");
+app.get("/login", (req, res) => {
+  res.render("pages/login.ejs");
 });
+
+let usuario = "";
+
+app.post("/login", (req, res) => {
+  const { body } = req;
+  usuario = body.usuario;
+  console.log(body.usuario);
+  res.render("pages/vistaProductos.ejs", { usuario });
+});
+
+app.get("/logout", (req, res) => {
+  //req.session.destroy((err) => {
+  //  if (err) {
+  //    return res.json({ status: "Logout ERROR", body: err });
+  //  }
+  res.render("pages/logout.ejs", { usuario });
+});
+//});
