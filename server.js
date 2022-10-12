43e3f0f8749ab9ff5696c7a6ea7a396392f78882
Nicolas Olmos <nicolas1819991@gmail.com>
2022-10-12 17:11:57 -0300

    vista de login y de register

--- server.js
+++ server.js
@@ -1,9 +1,100 @@
 const express = require("express");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const Usuarios = require("./models/usuarios");
+
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
 const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 const app = express();
 const PORT = 8080;
+
+function isValidPassword(user, password) {
+  return bcrypt.compareSync(password, user.password);
+}
+
+function createHash(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+}
+
+mongoose
+  .connect(
+    "mongodb+srv://Nicolas9412:admin123@cluster0.x4k71fz.mongodb.net/ecommerce?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected to DB"))
+  .catch((e) => {
+    console.error(e);
+    throw "can not connect to the db";
+  });
+
+passport.use(
+  "login",
+  new LocalStrategy((username, password, done) => {
+    Usuarios.findOne({ username }, (err, user) => {
+      if (err) return done(err);
+
+      if (!user) {
+        console.log("User Not Found with username " + username);
+        return done(null, false);
+      }
+
+      if (!isValidPassword(user, password)) {
+        console.log("Invalid Password");
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
+  })
+);
+
+passport.use(
+  "signup",
+  new LocalStrategy(
+    {
+      passReqToCallback: true,
+    },
+    (req, username, password, done) => {
+      Usuarios.findOne({ username: username }, function (err, user) {
+        if (err) {
+          console.log("Error in SignUp: " + err);
+          return done(err);
+        }
+
+        if (user) {
+          console.log("User already exists");
+          return done(null, false);
+        }
+
+        const newUser = {
+          username: username,
+          password: createHash(password),
+        };
+        Usuarios.create(newUser, (err, userWithId) => {
+          if (err) {
+            console.log("Error in Saving user: " + err);
+            return done(err);
+          }
+          console.log(user);
+          console.log("User Registration succesful");
+          return done(null, userWithId);
+        });
+      });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  Usuarios.findById(id, done);
+});
 
 app.use(
   session({
@@ -13,9 +104,10 @@ app.use(
       mongoOptions: advancedOptions,
     }),
     secret: "top secret",
-    resave: false,
+    resave: true,
+    rolling: true,
     saveUninitialized: false,
-    cookie: { maxAge: 600000 },
+    cookie: { maxAge: 600000, httpOnly: false, secure: false },
   })
 );
 
@@ -25,6 +117,9 @@ app.use(function (req, res, next) {
   req.session.touch();
   return next();
 });
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -48,6 +143,10 @@ function checkLogin(req, res, next) {
   }
   return next();
 }
+
+app.get("/register", (req, res) => {
+  return res.render("pages/register.ejs");
+});
 
 app.get("/login", checkLogin, (req, res) => {
   usuario = req.session["login"].usuario;
